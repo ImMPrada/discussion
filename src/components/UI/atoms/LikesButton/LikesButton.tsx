e90988bc446741a168313a03dd28880a97284fd2
immprada <im.mprada@gmail.com>
2022-02-11 05:25:16 -0500

    Mockup LikesButton atom

--- src/components/UI/atoms/LikesButton/LikesButton.tsx
+++ src/components/UI/atoms/LikesButton/LikesButton.tsx
@@ -1,8 +1,31 @@
 import React from 'react';
+import './styles.css'
 
 
-const LikesButton = () => (
-  <h1>HOLI</h1>
-)
+const LikesButton = () => {
+  const [value, setValue] = React.useState(0)
+  const increaseValue = () => {
+    let tempValue = value+1
+    setValue(tempValue)
+  }
+  const decreaseValue = () => {
+    let tempValue = Math.max(0, value-1)
+    setValue(tempValue)
+  }
+
+  return (
+    <div className="button-container">
+      <div className="button-container-action" onClick={increaseValue}>
+        <button>+</button>
+      </div>
+      <div className="button-container-counter">
+        <p>{value}</p>
+      </div>
+      <div className="button-container-action" onClick={decreaseValue}>
+      <button>-</button>
+      </div>
+    </div>
+  )
+}
 
 export default LikesButton
