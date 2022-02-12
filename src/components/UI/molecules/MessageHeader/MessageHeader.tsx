32649dafca94881bbdb68feb95c1bf50524652ef
immprada <im.mprada@gmail.com>
2022-02-11 19:07:37 -0500

    MessageHeader mockup

--- src/components/UI/molecules/MessageHeader/MessageHeader.tsx
+++ src/components/UI/molecules/MessageHeader/MessageHeader.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 import { ProfileBox, ATimeAgoLabel } from '../../atoms';
+import './styles.css'
 
 const MessageHeader = () => {
 
   return(
-    <>
-      <ProfileBox />
-      <ATimeAgoLabel label="lorem ipsum"/>
-    </>
+    <div className="message-header">
+      <div className="message-header-message-info">
+        <ProfileBox />
+        <ATimeAgoLabel label="lorem ipsum"/>
+      </div>
+      <div className="message-header-message-actions">
+        <h2>Holi</h2>
+      </div>
+    </div>
   )
 }
 
