0ebdc87e1f7eba89d24fd2a37069ea0e8d993244
immprada <im.mprada@gmail.com>
2022-02-11 19:25:28 -0500

    Message molecule mockup

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { MessageHeader } from './components/UI/molecules'
+import { Message } from './components/UI/molecules'
 
 function App() {
   return (
-    <MessageHeader />
+    <Message />
   );
 }
 
