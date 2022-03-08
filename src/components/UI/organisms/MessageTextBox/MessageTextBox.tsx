ac55fb774f89f63d57b16237878da6ffc22e9c83
miguel-prada <miguel@modernadvisor.ca>
2022-03-08 07:50:33 -0500

    Mockup MessageTextBox component

---
+++ src/components/UI/organisms/MessageTextBox/MessageTextBox.tsx
@@ -0,0 +1,32 @@
+import React from 'react';
+
+import './styles.css'
+import { ProfileBox } from '../../atoms';
+import { TextArea } from '../../atoms'
+import { Button } from '../../atoms'
+
+const MessageTextBox = () => {
+
+  return(
+    <div className="message-textbox">
+      <div className="message-textbox-profile-avatar">
+        <ProfileBox 
+          onlyAvatar = {true}
+          isYou = {false}
+        />
+      </div>
+      <div className="message-textbox-text">
+        <TextArea 
+          initialValue = ' Add a comment...'
+        />
+      </div>
+      <div className="message-textbox-button">
+        <Button 
+          label = 'Send'
+        />
+      </div>
+    </div>
+  )
+}
+
+export default MessageTextBox;
