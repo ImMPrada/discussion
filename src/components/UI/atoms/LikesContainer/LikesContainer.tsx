c07b39f9ad6c731476c4d09bce5d85b2a66112ce
miguel-prada <miguel@modernadvisor.ca>
2022-04-08 17:09:34 -0500

    Styles to LikesContainer componnent

--- src/components/UI/atoms/LikesContainer/LikesContainer.tsx
+++ src/components/UI/atoms/LikesContainer/LikesContainer.tsx
@@ -1,29 +1,29 @@
 import React from 'react';
+import { PlusIcon, MinusIcon } from '../../../icons';
 import './styles.scss'
 
 
 const LikesContainer = () => {
   const [value, setValue] = React.useState(0)
+
   const increaseValue = () => {
-    let tempValue = value+1
-    setValue(tempValue)
+    setValue(value + 1)
   }
   const decreaseValue = () => {
-    let tempValue = Math.max(0, value-1)
-    setValue(tempValue)
+    setValue(Math.max(0, value-1))
   }
 
   return (
-    <div className="button-container">
-      <div className="button-container-action" onClick={increaseValue}>
-        <button>+</button>
+    <div className="likes-container">
+      <button className="likes-container-action" onClick={increaseValue}>
+        <PlusIcon />
+      </button>
+      <div className="likes-container-counter">
+        <span>{value}</span>
       </div>
-      <div className="button-container-counter">
-        <p>{value}</p>
-      </div>
-      <div className="button-container-action" onClick={decreaseValue}>
-      <button>-</button>
-      </div>
+      <button className="likes-container-action" onClick={decreaseValue}>
+        <MinusIcon />
+      </button>
     </div>
   )
 }
