8abd699f0229f88ce22254147164e7be116210f6
ImMPrada <im.mprada@gmail.com>
2022-04-09 07:19:54 -0500

    Add VerticalRectangleIcon

---
+++ src/components/icons/VerticalRectangleIcon/VerticalRectangleIcon.tsx
@@ -0,0 +1,7 @@
+const VerticalRectangleIcon = () => (
+  <svg viewBox="0 0 2 348" fill="none" xmlns="http://www.w3.org/2000/svg">
+    <rect width="2" fill="#E9EBF0"/>
+  </svg>
+)
+
+export default VerticalRectangleIcon
