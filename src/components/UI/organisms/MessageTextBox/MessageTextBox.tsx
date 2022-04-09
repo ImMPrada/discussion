fe03da7d8312c4d9a671f305d6c4e2c5f2ee6bb1
ImMPrada <im.mprada@gmail.com>
2022-04-09 14:54:48 -0500

    Styles to MessageTextBox

--- src/components/UI/organisms/MessageTextBox/MessageTextBox.tsx
+++ src/components/UI/organisms/MessageTextBox/MessageTextBox.tsx
@@ -1,25 +1,21 @@
 import { profileData } from '../../../../mocks'
 
 import './styles.scss'
-import { TextArea, Avatar, Button } from '../../atoms'
+import { Avatar } from '../../atoms'
 
 const MessageTextBox = () => {
 
   return(
     <div className="message-textbox">
-      <div className="message-textbox-profile-avatar">
-        <Avatar profileImg={profileData.userAvatar}/>
+
+      <div className="message-textbox-avatar">
+        <Avatar profileImg={profileData.userAvatar} />
       </div>
-      <div className="message-textbox-text">
-        <TextArea 
-          initialValue = ' Add a comment...'
-        />
-      </div>
+      <textarea>Holi</textarea>
       <div className="message-textbox-button">
-        <Button 
-          label = 'Send'
-        />
+        <button>Send</button>
       </div>
+
     </div>
   )
 }
