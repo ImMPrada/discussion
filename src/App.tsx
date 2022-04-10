fd8d2abfd401cc6d7bb6b274f66b370b8825bb19
ImMPrada <im.mprada@gmail.com>
2022-04-10 08:35:42 -0500

    Implement of navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
 import { AuthProvider } from './contexts/AuthContext'
 import { DataProvider } from './contexts/DataContext';
-import { Wall } from './components/UI/organisms'
-import { MessageTextBox } from './components/UI/organisms';
-import './styles/global/app.scss'
+import AppRouter from './navigation/AppRouter/AppRouter';
 
 
 function App() {
   return (
     <AuthProvider>
       <DataProvider>
-        <div className='publications-container'>
-          <div className='publications-container-wall'>
-            <Wall/>
-          </div>
-          <div className='publications-container-message-box'>
-            <MessageTextBox/>
-          </div>
-        </div>
+        <AppRouter/>
       </DataProvider>
     </AuthProvider>
     
